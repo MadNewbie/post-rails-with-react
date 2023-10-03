02c8ca8df71962253a3868db633a040ac827be59
MadNewbie <rachmad.y25@gmail.com>
2023-10-03 12:29:55 +0700

    Add routing and post show page

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,16 +1,19 @@
+import {BrowserRouter as Router} from 'react-router-dom';
 import { useState } from 'react'
 import './App.css'
-import PostsLists from './features/posts/PostsList';
+import AppRoutes from './components/AppRoutes';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <Router>
       <div className='app'>
         <h1>React on Rails Blog</h1>
         <p>Find this application layout in fe/src/App.jsx</p>
-        <PostsLists/>
+        <Navbar/>
+        <AppRoutes/>
       </div>
-    </>
+    </Router>
   )
 }
 
